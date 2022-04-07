068960df6d8be1a6fee89e248f068801af0bf4f3
Utsav Anand <utsavfrutu@gmail.com>
2022-04-07 11:38:21 +0530

    Responsive layout (till services section)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,76 +15,155 @@ let t1 = gsap.timeline({
     start: 'top top',
     end: '+=1200%',
     pin: 'header',
-    scrub: true,
+    scrub: 0,
   }
 });
 
-t1.to('#car-img', {
-    x: '-74vw',
-  })
-  .to('#transition-txt', {
-    opacity: 1,
-    duration: 0,
-  })
-  .to('.tagline', {
-    opacity: 0,
-    duration: 0,
-  })
-  .to('#car-img', {
-    x: '-150vw',
-  })
-  .to('#services-section', {
-    display: 'block',
-    duration: 0,
-  })
-  .to('.tail-light', {
-    scale: 35,
-    duration: 0.8,
-  })
-  .to('#scroll', {
-    opacity: 0,
-    duration: 0,
-  })
-  .to('.color-shift', {
-    opacity: 1,
-    color: '#fff'
-  }, 1.2)
 
-  .fromTo('#service-1', {
-    opacity: 0,
-    y: 20,
-  }, {
-    opacity: 1,
-    y: 0
-  })
-  .to('#service-1', {
-    opacity: 0,
-    y: -20,
-  })
+// For desktop and tablet 
+if (window.innerWidth > 768) {
+  t1
+    .to('#car-img', {
+      x: '-74vw',
+    })
+    .to('#transition-txt', {
+      opacity: 1,
+      duration: 0,
+    })
+    .to('.tagline', {
+      opacity: 0,
+      duration: 0,
+    })
+    .to('#car-img', {
+      x: '-150vw',
+    })
+    .to('#services-section', {
+      display: 'block',
+      duration: 0,
+    })
+    .to('.tail-light', {
+      scale: 35,
+      duration: 0.8,
+    })
+    .to('#scroll', {
+      opacity: 0,
+      duration: 0,
+    })
+    .to('.color-shift', {
+      opacity: 1,
+      color: '#fff'
+    }, 1.2)
 
-  .fromTo('#service-2', {
-    opacity: 0,
-    y: 20,
-  }, {
-    opacity: 1,
-    y: 0
-  })
-  .to('#service-2', {
-    opacity: 0,
-    y: -20,
-  })
+    .fromTo('#service-1', {
+      opacity: 0,
+      y: 20,
+    }, {
+      opacity: 1,
+      y: 0
+    })
+    .to('#service-1', {
+      opacity: 0,
+      y: -20,
+    })
 
-  .fromTo('#service-3', {
-    opacity: 0,
-    y: 20,
-  }, {
-    opacity: 1,
-    y: 0
-  })
-  .to('#service-3', {
-    opacity: 0,
-    y: -20,
-  })
+    .fromTo('#service-2', {
+      opacity: 0,
+      y: 20,
+    }, {
+      opacity: 1,
+      y: 0
+    })
+    .to('#service-2', {
+      opacity: 0,
+      y: -20,
+    })
+
+    .fromTo('#service-3', {
+      opacity: 0,
+      y: 20,
+    }, {
+      opacity: 1,
+      y: 0
+    })
+    .to('#service-3', {
+      opacity: 0,
+      y: -20,
+    })
+}
+
+// For mobile
+else {
+  t1
+    .to('#car-img', {
+      x: '-128vw',
+    })
+    .to('#transition-txt', {
+      opacity: 1,
+      duration: 0,
+    })
+    .to('.tagline', {
+      opacity: 0,
+      duration: 0,
+    })
+    .to('#car-img', {
+      x: '-280vw',
+    })
+    .to('#services-section', {
+      display: 'block',
+      duration: 0,
+    })
+    .to('.tail-light', {
+      scale: 45,
+      duration: 0.8,
+    })
+    .to('#scroll', {
+      opacity: 0,
+      duration: 0,
+    })
+    .to('.color-shift', {
+      opacity: 1,
+      color: '#fff'
+    }, 1.2)
+
+    .fromTo('#service-1', {
+      opacity: 0,
+      y: 20,
+    }, {
+      opacity: 1,
+      y: 0
+    })
+    .to('#service-1', {
+      opacity: 0,
+      y: -20,
+    })
+
+    .fromTo('#service-2', {
+      opacity: 0,
+      y: 20,
+    }, {
+      opacity: 1,
+      y: 0
+    })
+    .to('#service-2', {
+      opacity: 0,
+      y: -20,
+    })
+
+    .fromTo('#service-3', {
+      opacity: 0,
+      y: 20,
+    }, {
+      opacity: 1,
+      y: 0
+    })
+    .to('#service-3', {
+      opacity: 0,
+      y: -20,
+    })
+
+}
+
+
 
 
 
