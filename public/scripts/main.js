cf974e2ab909e23cb230b21d261f89dfdfcc5565
Utsav Anand <utsavfrutu@gmail.com>
2022-04-07 12:28:05 +0530

    JS optimized

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -17,174 +17,90 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 }
 
 
-
-// For desktop and tablet 
+let isDesktop = false;
 if (window.innerWidth > 768) {
-
-
-  let t1 = gsap.timeline({
-    scrollTrigger: {
-      trigger: 'header',
-      start: 'top top',
-      end: '+=1200%',
-      pin: 'header',
-      scrub: 0,
-    }
-  });
-
-  t1
-    .to('#car-img', {
-      x: '-74vw',
-    })
-    .to('#transition-txt', {
-      opacity: 1,
-      duration: 0,
-    })
-    .to('.tagline', {
-      opacity: 0,
-      duration: 0,
-    })
-    .to('#car-img', {
-      x: '-150vw',
-    })
-    .to('#services-section', {
-      display: 'block',
-      duration: 0,
-    })
-    .to('.tail-light', {
-      scale: 35,
-      duration: 0.8,
-    })
-    .to('#scroll', {
-      opacity: 0,
-      duration: 0,
-    })
-    .to('.color-shift', {
-      opacity: 1,
-      color: '#fff'
-    }, 1.2)
-
-    .fromTo('#service-1', {
-      opacity: 0,
-      y: 20,
-    }, {
-      opacity: 1,
-      y: 0
-    })
-    .to('#service-1', {
-      opacity: 0,
-      y: -20,
-    })
-
-    .fromTo('#service-2', {
-      opacity: 0,
-      y: 20,
-    }, {
-      opacity: 1,
-      y: 0
-    })
-    .to('#service-2', {
-      opacity: 0,
-      y: -20,
-    })
-
-    .fromTo('#service-3', {
-      opacity: 0,
-      y: 20,
-    }, {
-      opacity: 1,
-      y: 0
-    })
-    .to('#service-3', {
-      opacity: 0,
-      y: -20,
-    })
+  isDesktop = true;
 }
 
-// For mobile
-else {
 
 
-  let t1 = gsap.timeline({
-    scrollTrigger: {
-      trigger: 'header',
-      start: 'top top',
-      end: '+=700%',
-      pin: 'header',
-      scrub: 0,
-    }
-  });
+let t1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: 'header',
+    start: 'top top',
+    end: isDesktop ? '+=1200%' : '+=700%',
+    pin: 'header',
+    scrub: 0,
+  }
+});
 
+t1
+  .to('#car-img', {
+    x: isDesktop ? '-74vw' : '-128vw',
+  })
+  .to('#transition-txt', {
+    opacity: 1,
+    duration: 0,
+  })
+  .to('.tagline', {
+    opacity: 0,
+    duration: 0,
+  })
+  .to('#car-img', {
+    x: isDesktop ? '-150vw' : '-280vw',
+  })
+  .to('#services-section', {
+    display: 'block',
+    duration: 0,
+  })
+  .to('.tail-light', {
+    scale: isDesktop ? 35 : 45,
+    duration: 0.8,
+  })
+  .to('#scroll', {
+    opacity: 0,
+    duration: 0,
+  })
+  .to('.color-shift', {
+    opacity: 1,
+    color: '#fff'
+  }, 1.2)
 
-  t1
-    .to('#car-img', {
-      x: '-128vw',
-    })
-    .to('#transition-txt', {
-      opacity: 1,
-      duration: 0,
-    })
-    .to('.tagline', {
-      opacity: 0,
-      duration: 0,
-    })
-    .to('#car-img', {
-      x: '-280vw',
-    })
-    .to('#services-section', {
-      display: 'block',
-      duration: 0,
-    })
-    .to('.tail-light', {
-      scale: 45,
-      duration: 0.8,
-    })
-    .to('#scroll', {
-      opacity: 0,
-      duration: 0,
-    })
-    .to('.color-shift', {
-      opacity: 1,
-      color: '#fff'
-    }, 1.2)
+  .fromTo('#service-1', {
+    opacity: 0,
+    y: 20,
+  }, {
+    opacity: 1,
+    y: 0
+  })
+  .to('#service-1', {
+    opacity: 0,
+    y: -20,
+  })
 
-    .fromTo('#service-1', {
-      opacity: 0,
-      y: 20,
-    }, {
-      opacity: 1,
-      y: 0
-    })
-    .to('#service-1', {
-      opacity: 0,
-      y: -20,
-    })
+  .fromTo('#service-2', {
+    opacity: 0,
+    y: 20,
+  }, {
+    opacity: 1,
+    y: 0
+  })
+  .to('#service-2', {
+    opacity: 0,
+    y: -20,
+  })
 
-    .fromTo('#service-2', {
-      opacity: 0,
-      y: 20,
-    }, {
-      opacity: 1,
-      y: 0
-    })
-    .to('#service-2', {
-      opacity: 0,
-      y: -20,
-    })
-
-    .fromTo('#service-3', {
-      opacity: 0,
-      y: 20,
-    }, {
-      opacity: 1,
-      y: 0
-    })
-    .to('#service-3', {
-      opacity: 0,
-      y: -20,
-    })
-
-}
+  .fromTo('#service-3', {
+    opacity: 0,
+    y: 20,
+  }, {
+    opacity: 1,
+    y: 0
+  })
+  .to('#service-3', {
+    opacity: 0,
+    y: -20,
+  })
 
 
 
